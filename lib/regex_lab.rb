805c4d505c023bbc77d33cfafe612ff972506542
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-02 00:04:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   words = text.split(" ")
   
   words.each do |word|
-    p word.scan(/^un + ing$/)
+    p word.scan(/^[u][n]+[i][n][g]$/)
   end
 end
 
