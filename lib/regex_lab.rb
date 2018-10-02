448135f2503b215ab34f63b9fa1642f26cd3cb7f
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-02 00:07:06 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  words = text.split(" ")
   
-  matched = words.map do |word|
-    p word.scan(//)
-  end
 end
 
 def words_five_letters_long(text)
